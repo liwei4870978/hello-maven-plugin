f7eb68e5d39284bafddd75a2ceacff7f085b5f70
晴天626 <1332142689@qq.com>
2024-09-04 14:45:24 +0800

    fix

--- src/main/java/com/atguigu/maven/MyHelloPlugin.java
+++ src/main/java/com/atguigu/maven/MyHelloPlugin.java
@@ -7,8 +7,10 @@ import org.apache.maven.plugins.annotations.Mojo;
 
 @Mojo(name = "sayHello")
 public class MyHelloPlugin extends AbstractMojo {
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("---> Hello, This is my first maven plugin. <---");
     }
+
 }
